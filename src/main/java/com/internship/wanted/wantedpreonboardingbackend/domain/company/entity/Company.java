552b9ddcfa0d1adb1c7386f56cc4511bcf59d166
sunsik <sunsig21c@gmail.com>
2023-10-15 07:01:30 +0900

    [feature-003-company] company entity 생성
    - base entity 상속
    - 공고 리스트 필드 추가할 예정

---
+++ src/main/java/com/internship/wanted/wantedpreonboardingbackend/domain/company/entity/Company.java
@@ -0,0 +1,25 @@
+package com.internship.wanted.wantedpreonboardingbackend.domain.company.entity;
+
+import com.internship.wanted.wantedpreonboardingbackend.domain.BaseEntity;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Company extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String companyName;
+	private String country;
+	private String region;
+}
