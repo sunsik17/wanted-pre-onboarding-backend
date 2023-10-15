67fa273a9a4a40d139b90d7feee0c301ddb14c35
sunsik <sunsig21c@gmail.com>
2023-10-15 18:55:55 +0900

    [feature-002-recruitment-1] 채용공고 등록 기능 개발
    채용공고 등록 기능 구현
    controller url : /recruitments/registration
    
    - 회사 id가 유효해야함
      - 회사를 찾지 못하면 runtimeException

---
+++ src/main/java/com/internship/wanted/wantedpreonboardingbackend/web/recruitment/RecruitmentController.java
@@ -0,0 +1,24 @@
+package com.internship.wanted.wantedpreonboardingbackend.web.recruitment;
+
+import com.internship.wanted.wantedpreonboardingbackend.domain.recruitment.dto.RecruitmentForm;
+import com.internship.wanted.wantedpreonboardingbackend.domain.recruitment.service.RecruitmentWriteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/recruitments")
+@RequiredArgsConstructor
+public class RecruitmentController {
+
+	private final RecruitmentWriteService recruitmentWriteService;
+
+	@PostMapping("/registration")
+	public ResponseEntity<?> registration(@RequestBody RecruitmentForm.Request request) {
+
+		return ResponseEntity.ok().body(recruitmentWriteService.registration(request));
+	}
+}
