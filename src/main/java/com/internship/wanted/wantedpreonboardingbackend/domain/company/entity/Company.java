7b9512bd87f26854a453e332d4262febf23cefd6
sunsik <sunsig21c@gmail.com>
2023-10-16 16:56:02 +0900

    [feature-004-recruitment_and_company_mapping-1] jpa를 이용해 회사와 채용공고 entity간 연관관계 매핑
    - 하나의 Company는 여러개의 Recruitment를 가질 수 있다.
    - Recruitment는 Company를 참조한다.
    - 조회 시 recruitment컬렉션 지연로딩한다
    - Company가 연관관계 관리한다

--- src/main/java/com/internship/wanted/wantedpreonboardingbackend/domain/company/entity/Company.java
+++ src/main/java/com/internship/wanted/wantedpreonboardingbackend/domain/company/entity/Company.java
@@ -1,11 +1,16 @@
 package com.internship.wanted.wantedpreonboardingbackend.domain.company.entity;
 
 import com.internship.wanted.wantedpreonboardingbackend.domain.BaseEntity;
+import com.internship.wanted.wantedpreonboardingbackend.domain.recruitment.entity.Recruitment;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -23,4 +28,7 @@ public class Company extends BaseEntity {
 
 	@Column(unique = true)
 	private String companyName;
+
+	@OneToMany(mappedBy = "recruitment", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	private List<Recruitment> recruitment;
 }
