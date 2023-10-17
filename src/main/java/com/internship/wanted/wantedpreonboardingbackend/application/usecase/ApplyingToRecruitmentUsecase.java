23dd41c019f00733863b68dc746fd53769a12a87
sunsik <sunsig21c@gmail.com>
2023-10-17 15:43:06 +0900

    [feature-008-company-2] ApplyingToRecruitmentUsecase class name 오탈자 수정

--- src/main/java/com/internship/wanted/wantedpreonboardingbackend/application/usecase/ApplyingToRecruitmentUsecase.java
+++ src/main/java/com/internship/wanted/wantedpreonboardingbackend/application/usecase/ApplyingToRecruitmentUsecase.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class ApplyingToRecruitmentUsercase {
+public class ApplyingToRecruitmentUsecase {
 
 	private final UserReadService userReadService;
 	private final RecruitmentReadService recruitmentReadService;
